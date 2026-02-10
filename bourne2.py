b569610ab6224a38028aeae9c7d86b746eeba01c
Michael Bradfield <michael.bradfield@takeda.com>
2026-02-10 17:13:22 -0500

    (mb) changed how variables are handled in bourne protocol pg

--- bourne2.py
+++ bourne2.py
@@ -1,27 +1,44 @@
 import streamlit as st
 import pandas as pd
 import functions as fx
+import inspect
 
 st.title("Bourne Protocol")
 
 st.image("assets/bourne_protocol.png", width=400) # "content"
 
 def reset_bourne_protocol():
+    # step results
     st.session_state.bourne_1_result = False
     st.session_state.bourne_2_result = False
     st.session_state.bourne_3_result = False
-    st.session_state.step_1_done = False
-    st.session_state.step_2_done = False
-    st.session_state.step_3_done = False
-    del st.session_state.bourne_1_conditions
-    del st.session_state.bourne_2_conditions
-    del st.session_state.bourne_3_conditions
+    # steps states
+    st.session_state.bourne_1_done = False
+    st.session_state.bourne_2_done = False
+    st.session_state.bourne_3_done = False
+    # step KPIs
+    st.session_state.bourne_1_kpi = [None, None, None]
+    st.session_state.bourne_2_kpi = [None, None, None]
+    st.session_state.bourne_3_kpi = [None, None, None]
+
+    # delete variables
+    vars_del = ['bourne_1_conditions', 'bourne_2_conditions', 'bourne_3_conditions',
+                'bourne_rpm', 'bourne_volume', 'bourne_feed',
+                'bourne_mid_rpm', 'bourne_mid_volume']
+    for var in vars_del:
+        if var in st.session_state:
+            # write var name to txt file for debugging
+            with open("debug.txt", "a") as f:
+                # get line number of code
+                f.write(f"Deleting variable: {var}\n")
+
+            del st.session_state[var]
 
 st.button("Reset Bourne Protocol", on_click=reset_bourne_protocol)
 
 st.divider()
 
-# get reactor varialbes
+# get reactor variables
 r = st.session_state.reactor
 
 if 'bourne_1_result' not in st.session_state:
@@ -30,14 +47,14 @@ if 'bourne_1_result' not in st.session_state:
 if 'bourne_2_result' not in st.session_state:
     st.session_state.bourne_2_result = False
 
-if 'step_1_done' not in st.session_state:
-    st.session_state.step_1_done = False
+if 'bourne_1_done' not in st.session_state:
+    st.session_state.bourne_1_done = False
 
-if 'step_2_done' not in st.session_state:
-    st.session_state.step_2_done = False
+if 'bourne_2_done' not in st.session_state:
+    st.session_state.bourne_2_done = False
 
-if 'step_3_done' not in st.session_state:
-    st.session_state.step_3_done = False
+if 'bourne_3_done' not in st.session_state:
+    st.session_state.bourne_3_done = False
 
 # ************************ STEP 1 ************************
 
@@ -48,31 +65,75 @@ col1, col2 = st.columns(2)
 # get stir speed range
 rpm_min = r[('Agitation Min', 'rpm')]
 rpm_max = r[('Agitation Max', 'rpm')]
-rpm_mid = (rpm_min + rpm_max) / 2
+
+# set default stirrer speed to midpoint of range
+if 'bourne_rpm' not in st.session_state:
+    st.session_state.bourne_rpm = [None, None, None]
+    st.session_state.bourne_rpm[1] = (rpm_min + rpm_max) / 2
+
+    # used for ensuring smooth update of variable
+    st.session_state.bourne_mid_rpm = str(st.session_state.bourne_rpm[1])
 
 # get reactor volume range
 V_min = r[('Volume Min', 'L')]
 V_max = r[('Volume Max', 'L')]
-V_mid = (V_min + V_max) / 2
 
-# get centerpoint conditions
-rpm_mid = float(col1.text_input("Centerpoint Stir Speed [rpm]", f"{rpm_mid}"))
-V_mid = float(col2.text_input("Centerpoint Volume [L]", f"{V_mid}"))
+# default volume to midpoint of range
+if 'bourne_volume' not in st.session_state:
+    st.session_state.bourne_volume = [None, None, None]
+    st.session_state.bourne_volume[1] = (V_min + V_max) / 2
+
+    # used for ensuring smooth update of variable
+    st.session_state.bourne_mid_volume = str(st.session_state.bourne_volume[1])
+
+# *************** variable update functions ****************
+
+def update_rpm():
+    try:
+        st.session_state.bourne_rpm[1] = float(st.session_state.bourne_mid_rpm)
+    except ValueError:
+        st.error("Invalid input for stir speed. Please enter a number.")
+
+def update_vol():
+    try:
+        st.session_state.bourne_volume[1] = float(st.session_state.bourne_mid_volume)
+    except ValueError:
+        st.error("Invalid input for volume. Please enter a number.")
+
+# ********************************************************
+
+# get centerpoint conditions; don't allow changes once step is done to preserve results
+if not st.session_state.bourne_1_done:
+
+    col1.text_input(
+    "Centerpoint Stir Speed [rpm]",
+    key="bourne_mid_rpm",
+    on_change=update_rpm
+    )
+
+    col2.text_input(
+    "Centerpoint Volume [L]",
+    key="bourne_mid_volume",
+    on_change=update_vol
+    )
 
 PV_factor = 10
 
 # Calculate rpm values based on 100x P/V changes
-rpm_low = round((1/PV_factor * rpm_mid**3)**(1/3), 0)
-rpm_high = round((PV_factor * rpm_mid**3)**(1/3), 0)
+st.session_state.bourne_rpm[0] = round((1/PV_factor * st.session_state.bourne_rpm[1]**3)**(1/3), 0)
+st.session_state.bourne_rpm[2] = round((PV_factor * st.session_state.bourne_rpm[1]**3)**(1/3), 0)
 
-# create df of conditions
-conditions = {'Volume [L]': [V_mid, V_mid, V_mid],
-            'Agitation [rpm]': [rpm_low, rpm_mid, rpm_high],
-            'KPI': [None, None, None]}
+# create dict of conditions and KPIs
+if 'bourne_1_kpi' not in st.session_state:
+     st.session_state.bourne_1_kpi = [None, None, None]
 
-# create new dataframe if not already in session state; otherwise use existing dataframe to preserve user input
-if 'bourne_1_conditions' not in st.session_state:
-    st.session_state.bourne_1_conditions = pd.DataFrame(conditions, index=[f"{1/PV_factor} P/V",
+conditions = {'Volume [L]': [st.session_state.bourne_volume[1],
+                             st.session_state.bourne_volume[1],
+                             st.session_state.bourne_volume[1]],
+            'Agitation [rpm]': st.session_state.bourne_rpm,
+             'KPI': st.session_state.bourne_1_kpi}
+
+st.session_state.bourne_1_conditions = pd.DataFrame(conditions, index=[f"{1/PV_factor} P/V",
                                                                         "P/V", f"{PV_factor} P/V"])
 
 def bourne_1_update():
@@ -83,6 +144,9 @@ def bourne_1_update():
         kpi_mid = float(df.loc["P/V", "KPI"])
         kpi_low = float(df.loc[f"{1/PV_factor} P/V", "KPI"])
         kpi_high = float(df.loc[f"{PV_factor} P/V", "KPI"])
+        # preserve KPI values
+        st.session_state.bourne_1_kpi = [kpi_low, kpi_mid, kpi_high]
+
     except:
         st.error("Error reading KPI values. Please ensure all KPI values are filled in.")
         return
@@ -94,22 +158,22 @@ def bourne_1_update():
         st.success("KPI is NOT sensitive to stir speed. Exit Bourne Protocol.")
         st.session_state.bourne_1_result = False
 
-    st.session_state.step_1_done = True
+    st.session_state.bourne_1_done = True
 
 # update placeholder variable with user input
 # ensure table becomes immutable once step is done to preserve results
-if st.session_state.step_1_done:
+if st.session_state.bourne_1_done:
     st.data_editor(st.session_state.bourne_1_conditions, num_rows="fixed", disabled=True)
 else:
     bourne_1_mod = st.data_editor(st.session_state.bourne_1_conditions, num_rows="fixed")
 
-if not st.session_state.step_1_done:
+if not st.session_state.bourne_1_done:
     st.button("Run Step 1", on_click=bourne_1_update)
 else:
     st.button("Run Step 1", disabled=True)
 
 # add result if Step 1 is done
-if st.session_state.step_1_done:
+if st.session_state.bourne_1_done:
 
     st.metric("Step 1 Result",
               "Mixing Sensitive" if st.session_state.bourne_1_result else "Not Mixing Sensitive",
@@ -123,8 +187,13 @@ st.subheader("Step 2 - Feed Rate Sensitivity")
 
 col1, col2 = st.columns(2)
 
+if 'feed_mid' not in st.session_state:
+    st.session_state.feed_mid = 0.1
+feed_mid = st.session_state.feed_mid
+
 # get centerpoint conditions
-feed_mid = float(col1.text_input("Centerpoint Feed Rate (Q) [kg/h]", f"{0.1}"))
+if not st.session_state.bourne_2_done:
+    feed_mid = float(col1.text_input("Centerpoint Feed Rate (Q) [kg/h]", f"{0.1}"))
 
 feed_factor = 3
 
@@ -132,17 +201,18 @@ feed_factor = 3
 feed_low = feed_mid / feed_factor
 feed_high = feed_factor * feed_mid
 
-rpm_mid = st.session_state.bourne_rpm
-V_mid = st.session_state.bourne_volume
+rpm_mid = st.session_state.bourne_rpm[1]
+V_mid = st.session_state.bourne_volume[1]
 
-# create df of conditions
+# create dict of conditions and KPIs
+if 'bourne_2_kpi' not in st.session_state:
+     st.session_state.bourne_2_kpi = [None, None, None]
 feed_conditions = {'Feed Rate [kg/h]': [feed_low, feed_mid, feed_high],
             'Agitation [rpm]': [rpm_mid, rpm_mid, rpm_mid],
             'Volume [L]': [V_mid, V_mid, V_mid],
-            'KPI': [None, None, None]}
+            'KPI': st.session_state.bourne_2_kpi}
 
-if 'bourne_2_conditions' not in st.session_state:
-    st.session_state.bourne_2_conditions = pd.DataFrame(feed_conditions, index=[f"{1/feed_factor} Q", "Q", f"{feed_factor} Q"])
+st.session_state.bourne_2_conditions = pd.DataFrame(feed_conditions, index=[f"{1/feed_factor} Q", "Q", f"{feed_factor} Q"])
 
 def bourne_2_update():
     st.session_state.bourne_2_conditions = bourne_2_mod.copy()
@@ -152,6 +222,8 @@ def bourne_2_update():
     kpi_mid = float(df.loc["Q", "KPI"])
     kpi_low = float(df.loc[f"{1/feed_factor} Q", "KPI"])
     kpi_high = float(df.loc[f"{feed_factor} Q", "KPI"])
+    # preserve KPI values
+    st.session_state.bourne_2_kpi = [kpi_low, kpi_mid, kpi_high]
 
     if abs(kpi_high-kpi_mid)/kpi_mid > 0.1 or abs(kpi_low - kpi_mid)/kpi_mid > 0.1:
         st.warning("KPI is sensitive to feed rate. Process is meso- or macromixing sensitive. Proceed to Step 3")
@@ -160,21 +232,21 @@ def bourne_2_update():
         st.success("KPI is NOT sensitive to feed rate. Process is micromixing sensitive.")
         st.session_state.bourne_2_result = False
 
-    st.session_state.step_2_done = True
+    st.session_state.bourne_2_done = True
 
 # ensure table becomes immutable once step is done to preserve results
-if st.session_state.step_2_done:
+if st.session_state.bourne_2_done:
     st.data_editor(st.session_state.bourne_2_conditions, num_rows="fixed", disabled=True)
 else:
     bourne_2_mod = st.data_editor(st.session_state.bourne_2_conditions, num_rows="fixed")
 
-if (not st.session_state.step_1_done) or (st.session_state.step_2_done):
+if (not st.session_state.bourne_1_done) or (st.session_state.bourne_2_done):
     st.button("Run Step 2", disabled=True)
 else:
     st.button("Run Step 2", on_click=bourne_2_update)
 
 # add result if Step 2 is done
-if st.session_state.step_2_done:
+if st.session_state.bourne_2_done:
 
     st.metric("Step 2 Result",
               "Meso-/Macromixing Sensitive" if st.session_state.bourne_2_result else "Micromixing Sensitive",
@@ -192,12 +264,16 @@ rpm_mid = st.session_state.bourne_rpm
 V_mid = st.session_state.bourne_volume
 
 # create df of conditions
-feed_location = {'Agitation [rpm]': [rpm_mid, rpm_mid, rpm_mid],
-                'Volume [L]': [V_mid, V_mid, V_mid],
-            'KPI': [None, None, None]}
+if 'bourne_3_kpi' not in st.session_state:
+     st.session_state.bourne_3_kpi = [None, None, None]
 
-if 'bourne_3_conditions' not in st.session_state:
-    st.session_state.bourne_3_conditions = pd.DataFrame(feed_location, index=["Surface", "Sub-surface", "Impeller Zone"])
+feed_location = {'Agitation [rpm]': st.session_state.bourne_rpm,
+                'Volume [L]': [st.session_state.bourne_volume[1],
+                               st.session_state.bourne_volume[1],
+                               st.session_state.bourne_volume[1]],
+            'KPI': st.session_state.bourne_3_kpi}
+
+st.session_state.bourne_3_conditions = pd.DataFrame(feed_location, index=["Surface", "Sub-surface", "Impeller Zone"])
 
 def bourne_3_update():
     st.session_state.bourne_3_conditions = bourne_3_mod.copy()
@@ -207,6 +283,8 @@ def bourne_3_update():
     kpi_mid = float(df.loc["Surface", "KPI"])
     kpi_low = float(df.loc[f"Sub-surface", "KPI"])
     kpi_high = float(df.loc[f"Impeller Zone", "KPI"])
+    # preserve KPI values
+    st.session_state.bourne_3_kpi = [kpi_low, kpi_mid, kpi_high]
 
     if abs(kpi_high-kpi_mid)/kpi_mid > 0.1 or abs(kpi_low - kpi_mid)/kpi_mid > 0.1:
         st.warning("KPI is sensitive to feed location. Process is mesomixing sensitive.")
@@ -215,21 +293,21 @@ def bourne_3_update():
         st.success("KPI is NOT sensitive to feed location. Process is macromixing sensitive.")
         st.session_state.bourne_3_result = False
 
-    st.session_state.step_3_done = True
+    st.session_state.bourne_3_done = True
 
 # ensure table becomes immutable once step is done to preserve results
-if st.session_state.step_3_done:
+if st.session_state.bourne_3_done:
     st.data_editor(st.session_state.bourne_3_conditions, num_rows="fixed", disabled=True)
 else:
     bourne_3_mod = st.data_editor(st.session_state.bourne_3_conditions, num_rows="fixed")
 
-if (not st.session_state.step_2_done) or (st.session_state.step_3_done):
+if (not st.session_state.bourne_2_done) or (st.session_state.bourne_3_done):
     st.button("Run Step 3", disabled=True)
 else:
     st.button("Run Step 3", on_click=bourne_3_update)
 
 # add result if Step 3 is done
-if st.session_state.step_3_done:
+if st.session_state.bourne_3_done:
 
     st.metric("Step 3 Result",
               "Mesomixing Sensitive" if st.session_state.bourne_3_result else "Macromixing Sensitive",
